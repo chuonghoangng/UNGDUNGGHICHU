9a94cfe446b214ad968e81caeb3016ea4d9e405c
ChuongHayHo <nguyenhoangchuong12345@gmail.com>
2021-05-02 23:21:55 +0700

    appghichu

---
+++ app/src/main/java/chuong/example/appghichudata/ChucNangNhacNho.java
@@ -0,0 +1,14 @@
+package chuong.example.appghichudata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChucNangNhacNho extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chuc_nang_nhac_nho);
+    }
+}
